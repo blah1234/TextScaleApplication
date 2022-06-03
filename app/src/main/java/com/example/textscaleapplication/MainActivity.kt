c83db75161e9b3b7286a0ed32950f8960263f11f
shawn hwang <shawn_hwang@apple.com>
2022-06-03 13:58:29 -0700

    cleanup

--- app/src/main/java/com/example/textscaleapplication/MainActivity.kt
+++ app/src/main/java/com/example/textscaleapplication/MainActivity.kt
@@ -18,51 +18,37 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        const val SCALE = 1.067f
+        const val SCALE_1 = 1.067f
+        const val SCALE_2 = 1.5f
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val world = findViewById<LinearLayout>(R.id.world)
-
-        val hello = "Hello World"
-        val inflater = LayoutInflater.from(this)
-        val views = mutableListOf<View>()
-        for (c in hello) {
-            val textView = inflater.inflate(R.layout.text, null)
-            (textView as TextView).apply {
-                text = c.toString()
-                paintFlags = Paint.FILTER_BITMAP_FLAG
-            }
-
-            world.addView(textView)
-            views.add(textView)
+        val world1 = findViewById<TextView>(R.id.world1)
+        val pressMe1 = findViewById<Button>(R.id.press1)
+        pressMe1.setOnClickListener {
+            val scaleX = ObjectAnimator.ofFloat(world1, View.SCALE_X, 1f, SCALE_1, 1f)
+            val scaleY = ObjectAnimator.ofFloat(world1, View.SCALE_Y, 1f, SCALE_1, 1f)
+            val set = AnimatorSet()
+            set.playTogether(scaleX, scaleY)
+            set.duration = 5000
+            set.interpolator = LinearInterpolator()
+            set.start()
         }
 
 
-        val pressMe = findViewById<Button>(R.id.press)
-        pressMe.setOnClickListener {
-//            for (i in views.indices) {
-//                val v = views[i]
-            val v = world
-                val scaleX = ObjectAnimator.ofFloat(v, View.SCALE_X, 1f, SCALE, 1f)
-                val scaleY = ObjectAnimator.ofFloat(v, View.SCALE_Y, 1f, SCALE, 1f)
-                val set = AnimatorSet()
-                set.playTogether(scaleX, scaleY)
-//                set.startDelay = i * 100L
-                set.duration = 3000
-                set.interpolator = LinearInterpolator()
-                set.start()
-//            }
-
-//            val scaleAnim = ScaleAnimation(1f, SCALE, 1f, SCALE, Animation.RELATIVE_TO_SELF, .5f, Animation.RELATIVE_TO_SELF, .5f)
-//            scaleAnim.interpolator = LinearInterpolator()
-//            scaleAnim.duration = 5000 /2
-//            scaleAnim.repeatCount = 1
-//            scaleAnim.repeatMode = Animation.REVERSE
-//            world.startAnimation(scaleAnim)
+        val world2 = findViewById<TextView>(R.id.world2)
+        val pressMe2 = findViewById<Button>(R.id.press2)
+        pressMe2.setOnClickListener {
+            val scaleX = ObjectAnimator.ofFloat(world2, View.SCALE_X, 1f, SCALE_2, 1f)
+            val scaleY = ObjectAnimator.ofFloat(world2, View.SCALE_Y, 1f, SCALE_2, 1f)
+            val set = AnimatorSet()
+            set.playTogether(scaleX, scaleY)
+            set.duration = 5000
+            set.interpolator = LinearInterpolator()
+            set.start()
         }
     }
 }
